0afbf030ab144d57f9a8a8bf4b06b9193a5be632
julian-c-dev <78905935+julian-c-dev@users.noreply.github.com>
2022-04-03 12:20:24 +0100

    Add flipCard function (simple flip when click)

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,10 +1,22 @@
 import React from "react";
 import "./Cards.css";
+import imageBlank from "../../assets/blank.png";
 
 const Cards = (props) => {
+  const flipCard = (e) => {
+    e.preventDefault();
+    e.currentTarget.src = props.character.image;
+  };
+
   return (
     <div>
-      <img className="cards" src={props.character.image} alt="" />
+      <img
+        id={props.character}
+        onClick={(e) => flipCard(e)}
+        className="cards"
+        src={imageBlank}
+        alt=""
+      />
     </div>
   );
 };
