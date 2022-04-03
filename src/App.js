da141fdad59542d7d87e87e995d458534b696d16
julian-c-dev <78905935+julian-c-dev@users.noreply.github.com>
2022-04-03 10:06:54 +0100

    fech caracters from api & store in character state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import "./App.css";
+import React, { useState, useEffect } from "react";
 import Board from "./components/Board/Board";
 import Cards from "./components/Cards/Cards";
 import Sign from "./components/Sign/Sign";
 
 function App() {
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    const getCharactersFromApi = () => {
+      return fetch("https://rickandmortyapi.com/api/character")
+        .then((res) => res.json())
+        .then((data) => setCharacters(data.results))
+        .catch((error) => console.log(error));
+    };
+    getCharactersFromApi();
+  }, []);
+
   return (
     <div className="container ">
       <h1 className="title"> The Rick &amp; Morty Memory Match Game </h1>
       <Board />
-      <Cards />
+      <Cards>{console.log(characters)}</Cards>
       <Cards />
       <Cards />
       <Sign />
